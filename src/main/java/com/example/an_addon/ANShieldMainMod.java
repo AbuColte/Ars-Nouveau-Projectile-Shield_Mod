5feff243e2750b391373f8b9477814cf206d1a2e
cxldy <94399549+AbuColte@users.noreply.github.com>
2025-04-05 21:50:27 +0000

    Update ANShieldMainMod.java

--- src/main/java/com/example/an_addon/ANShieldMainMod.java
+++ src/main/java/com/example/an_addon/ANShieldMainMod.java
@@ -14,7 +14,6 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(ANShieldMainMod.MODID)
 public class ANShieldMainMod
 {
@@ -44,10 +43,8 @@ public class ANShieldMainMod
 
     }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
-        // do something when the server starts
         LOGGER.info("HELLO from server starting");
     }
 
